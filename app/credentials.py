a5f780265825224918456508b72173e814d18379
Ethan Winters <ewinters@terpmail.umd.edu>
2017-06-24 15:04:23 -0400

    App put together

---
+++ app/credentials.py
@@ -0,0 +1,4 @@
+account_sid = 'ACe2382a6df1ddb357cb9e196dfc5974c8'
+auth_token = 'b69915c997743072fcee4faf5ab989b9'
+my_cell = '+18477678823'
+my_twilio = '+18472789860'
---
+++ app/send_sms.py
@@ -0,0 +1,28 @@
+from twilio.rest import Client
+from credentials import account_sid, auth_token, my_cell, my_twilio
+import forecastio
+import schedule
+import time
+
+"""Get weather for curent day"""
+api_key = 'df81a794c85818fc1e3dd5140d394986'
+lat = 38.958631
+lng = -77.357003
+forecast = forecastio.load_forecast(api_key, lat, lng)
+daily_summary = forecast.daily().summary
+
+
+"""Set up function to create and send text"""
+client = Client(account_sid, auth_token)
+msg = 'Hi Ethan, here is a summary of the days weather:\n' + daily_summary
+def send_text():
+	text = client.messages.create(to=my_cell, from_=my_twilio, body=msg)
+	print ('message sent')
+
+"""schedule texts"""
+schedule.every(1).minutes.do(send_text)
+
+while True:
+	schedule.run_pending()
+	time.sleep(60)
+
